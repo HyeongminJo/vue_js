86dd39e59ee5c25eaa8b540ae729b4efe301dfd3
HyeongminJo <zzz659@naver.com>
2023-10-06 09:02:24 +0900

    기본적인 옵션 구성
    231006

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var app = new Vue({
     el: '#app',
+    created: function() {
+        //이 인스턴스의 생성과 초기화가 종료되었을 때
+        console.log('create')
+    },
     data: {
         message:'Hello Vue.js!',
         list: ['사과', '바나나', '딸기'],
@@ -8,6 +12,12 @@ var app = new Vue({
     methods: {
         handleClick:function(event) {
             alert(event.target)
+        }
+    },
+    computed: {
+        computedMessage: function() {
+            //어떤 처리를 해서 결과 리턴하기
+            return this.message + '!'
         }
     }
 })
