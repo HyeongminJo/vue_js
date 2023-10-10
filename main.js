e235da42ca5f5b6f7407eef164ff66a7c5289b8f
HyeongminJo <zzz659@naver.com>
2023-10-10 18:58:21 +0900

    231010
    231010

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ var app = new Vue({
         console.log('create')
     },
     data: {
-        message:'Hello Vue.js!',
+        message: {
+            value: 'Hello Vue.js!'
+        },
         list: ['사과', '바나나', '딸기'],
         show: true,
         newTodoText: '',
         visitCount: 0,
         hideCompleted: false,
         todos: [],
-        error: null
+        error: null,
+        num: 1
     },
     methods: {
         handleClick:function(event) {
